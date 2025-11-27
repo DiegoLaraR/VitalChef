0ad97893544fe5ce8563c384207c563ed3e703bb
Diego Lara <diegoantoniolarareyes95@gmail.com>
2025-11-26 21:44:36 -0300

    Cambios pequeños

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Vital Chef'),
+        actions: [SearchBar(leading: const Icon(Icons.search))],
+      ),
+      body: Center(),
+    );
+  }
+}
